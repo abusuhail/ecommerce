bcb78c1d617d6f75f7038a2bce30ed06b34a0eb8
abusuhail <137296833+abusuhail@users.noreply.github.com>
2023-08-02 22:00:30 +0700

    login API

---
+++ src/app/api/v1/auth/login/route.js
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/utils/prisma";
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+
+export async function POST(req) {
+  const { email, password } = await req.json();
+
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (user) {
+      const isPasswordMatched = await compare(password, user.password);
+
+      if (isPasswordMatched) {
+        const userData = {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          no_hp: user.no_hp,
+        };
+
+        const token = sign(userData, process.env.TOKEN_SECRET_KEY);
+        return NextResponse.json({ record: userData, token }, { status: 200 });
+      }
+      return NextResponse.json(
+        { message: "email dan password salah!" },
+        { status: 403 }
+      );
+    }
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error }, { status: error.status });
+  }
+}
